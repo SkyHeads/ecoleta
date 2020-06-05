9508e95f2c187cfb55cb7bbf02ae31a3cac17ba9
Bruno Fernandes <skyheads@live.com>
2020-06-04 21:50:07 -0300

    add ts-node/ts-node-dev and adjust scripts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,9 @@
-console.log('Teste')
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ ok: true });
+});
+
+app.listen(3333);
