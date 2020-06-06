94922852add6081026352e6f264251419e492e7d
Bruno Fernandes <skyheads@live.com>
2020-06-06 00:54:27 -0300

    create controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,9 @@
 import { Router } from 'express';
-import knex from './database/connection';
 
 const route = Router();
 
-route.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
+route.get('/items', );
 
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      name: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(serializedItems);
-});
-
-route.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id
-    };
-  });
-
-  await trx('point_items').insert(pointItems);
-
-  return response.json({ success: true });
-});
+route.post('/points', );
 
 export default route;
