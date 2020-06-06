057cf7c19ff1252fc2408c231373abb60414f5c3
Bruno Fernandes <skyheads@live.com>
2020-06-06 00:39:11 -0300

    initial points route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,39 @@ route.get('/items', async (request, response) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       name: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return response.json(serializedItems);
+});
+
+route.post('/points', async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const createdPoint = await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  })
+
+  return response.json({ success: true });
 });
 
 export default route;
