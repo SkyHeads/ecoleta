122e0d49f302eee1b7c705fbc96e1cddce858822
Bruno Fernandes <skyheads@live.com>
2020-06-10 00:58:11 -0300

    header create point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint: React.FC = () => {
   return (
-    <h1>CreatePoint</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+    </div>
   );
 };
 
