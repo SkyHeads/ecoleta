b0030fc2578cbea55a2126ca9ca2f304a7e8a7c5
Bruno Fernandes <skyheads@live.com>
2020-06-10 00:30:16 -0300

    main landing page html

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -11,6 +11,18 @@ const Home: React.FC = () => {
         <header>
           <img src={logo} alt="Ecoleta"/>
         </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <a href="/cadastro">
+            <span>
+              >
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
       </div>
     </div>
   );
