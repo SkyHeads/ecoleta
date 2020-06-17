0616e1bec31b5bdb97eb2548a045df875e9f7a8d
Bruno Fernandes <skyheads@live.com>
2020-06-16 22:54:12 -0300

    import reactbutton and vector icons

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
 import { View, Image, StyleSheet, Text, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Home: React.FC = () => {
   return (
@@ -12,6 +14,19 @@ const Home: React.FC = () => {
         <Image source={require('../../assets/logo.png')} />
         <Text style={styles.title}>Seu Marketplace de coleta de resíduos</Text>
         <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
       </View>
     </ImageBackground>
   );
