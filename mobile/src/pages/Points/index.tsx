a31503860901728da3ee425c127805229873290f
Bruno Fernandes <skyheads@live.com>
2020-06-16 23:30:49 -0300

    create scrollview to items/react-native-svg

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, ScrollView } from 'react-native';
 import MapView from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 
 const Points: React.FC = () => {
   const navigation = useNavigation();
@@ -13,22 +14,61 @@ const Points: React.FC = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleNavigateBack}>
-        <Icon name="arrow-left" size={20} color="#34cb79"/>
-      </TouchableOpacity>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
 
-      <Text style={styles.title}>
-        Bem vindo.
-      </Text>
-      <Text style={styles.description}>
-        Encontre no mapa um ponto de coleta.
-      </Text>
+        <Text style={styles.title}>
+          Bem vindo.
+        </Text>
+        <Text style={styles.description}>
+          Encontre no mapa um ponto de coleta.
+        </Text>
 
-      <View style={styles.mapContainer}>
-        <MapView style={styles.map} />
+        <View style={styles.mapContainer}>
+          <MapView style={styles.map} />
+        </View>
       </View>
-    </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView 
+          horizontal 
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.13:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.13:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.13:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.13:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.13:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.0.13:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>
   )
 };
 
