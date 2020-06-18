6153fb3d94061b3062c5fbed5c317b0c4b269844
Bruno Fernandes <skyheads@live.com>
2020-06-18 00:02:00 -0300

    handleselectitem and css selecteditem

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -15,6 +15,7 @@ interface Item {
 
 const Points: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
   const navigation = useNavigation();
   
   useEffect(() => {
@@ -29,6 +30,18 @@ const Points: React.FC = () => {
 
   function handleNavigateToDetail() {
     navigation.navigate('Detail');
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([ ...selectedItems, id ]);
+    }
   }
 
   return (
@@ -83,8 +96,11 @@ const Points: React.FC = () => {
           {items.map(item => (
             <TouchableOpacity 
               key={String(item.id)} 
-              style={styles.item} 
-              onPress={() => {}}
+              style={[
+                styles.item,
+                selectedItems.includes(item.id) ? styles.selectedItem : {},
+              ]} 
+              onPress={() => handleSelectItem(item.id)}
               activeOpacity={0.6}
             >
               <SvgUri width={42} height={42} uri={item.image_url}/>
