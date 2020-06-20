b7762433aff14648f536b73a511661807e4595e9
Bruno Fernandes <skyheads@live.com>
2020-06-20 14:44:28 -0300

    formdata createpoint finish and call api post

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -32,6 +32,7 @@ const CreatePoint: React.FC = () => {
   const [selectedUf, setSelectedUf] = useState('0');
   const [cities, setCities] = useState<string[]>([]);
   const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const [formData, setFormData] = useState({
     name: '',
@@ -129,16 +130,21 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {  
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    const data = new FormData();
+    
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+    
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
+
     await api.post('points', data);
 
     alert('Ponto de coleta criado!');
@@ -160,7 +166,9 @@ const CreatePoint: React.FC = () => {
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
         
-        <Dropzone />
+        <Dropzone
+          onFileUploaded={setSelectedFile}
+        />
 
         <fieldset>
           <legend>
